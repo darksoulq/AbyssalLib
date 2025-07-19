9d13371c96aed6fc0b27fb13af68c9fd553dfc1b
darksoulq <amaanwani1a@gmail.com>
2025-07-19 05:43:26 +0530

    Fixes: - Gui click eventts

--- src/main/java/com/github/darksoulq/abyssallib/server/event/internal/GuiEvents.java
+++ src/main/java/com/github/darksoulq/abyssallib/server/event/internal/GuiEvents.java
@@ -1,17 +1,16 @@
 package com.github.darksoulq.abyssallib.server.event.internal;
 
-import com.github.darksoulq.abyssallib.AbyssalLib;
 import com.github.darksoulq.abyssallib.server.event.ActionResult;
 import com.github.darksoulq.abyssallib.server.event.SubscribeEvent;
 import com.github.darksoulq.abyssallib.world.level.inventory.gui.GuiElement;
 import com.github.darksoulq.abyssallib.world.level.inventory.gui.GuiManager;
 import com.github.darksoulq.abyssallib.world.level.inventory.gui.GuiView;
-import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
 
 public class GuiEvents {
+
     @SubscribeEvent
     public void onClick(InventoryClickEvent event) {
         GuiView view = GuiManager.openViews.get(event.getView());
@@ -20,15 +19,15 @@ public class GuiEvents {
         int rawSlot = event.getRawSlot();
         boolean top = rawSlot < view.getTop().getSize();
         GuiView.Segment segment = top ? GuiView.Segment.TOP : GuiView.Segment.BOTTOM;
+        int slot = event.getSlot();
 
-        GuiElement element = view.getElementAt(segment, rawSlot);
+        GuiElement element = view.getElementAt(segment, slot);
         if (element == null) {
             event.setCancelled(true);
             return;
         }
 
-        ActionResult result = element.onClick(view, rawSlot, event.getClick(), event.getCursor(),
-                event.getCurrentItem());
+        ActionResult result = element.onClick(view, slot, event.getClick(), event.getCursor(), event.getCurrentItem());
         event.setCancelled(result == ActionResult.CANCEL);
     }
 
@@ -41,8 +40,9 @@ public class GuiEvents {
         for (int rawSlot : event.getRawSlots()) {
             boolean top = rawSlot < view.getTop().getSize();
             GuiView.Segment segment = top ? GuiView.Segment.TOP : GuiView.Segment.BOTTOM;
+            int slot = view.getInventoryView().convertSlot(rawSlot);
 
-            GuiElement el = view.getElementAt(segment, rawSlot);
+            GuiElement el = view.getElementAt(segment, slot);
             if (el == null || el.onDrag(view, event.getNewItems()) == ActionResult.CANCEL) {
                 cancel = true;
             }
