cce81a6d6b504e7f1f972c4712e14bba2096de81
darksoulq <amaanwani1a@gmail.com>
2025-07-21 11:25:40 +0530

    New: - GuiFlag for certain uses
    Fixed:
    - Final TextOffset fix.
    - Guis work properly now

--- src/main/java/com/github/darksoulq/abyssallib/world/level/inventory/gui/AbstractGui.java
+++ src/main/java/com/github/darksoulq/abyssallib/world/level/inventory/gui/AbstractGui.java
@@ -3,13 +3,15 @@ package com.github.darksoulq.abyssallib.world.level.inventory.gui;
 import net.kyori.adventure.text.Component;
 import org.bukkit.inventory.MenuType;
 
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 
 public abstract class AbstractGui extends Gui {
 
     public AbstractGui(MenuType menuType, Component title) {
-        super(menuType, title, new HashMap<>(), new LinkedList<>(), new LinkedList<>(), null, null);
+        super(menuType, title, new HashMap<>(), new LinkedList<>(), new LinkedList<>(), EnumSet.noneOf(GuiFlag.class),null, null);
         init();
         this.onOpen = this::onOpen;
         this.onClose = this::onClose;
@@ -21,6 +23,12 @@ public abstract class AbstractGui extends Gui {
 
     public void set(SlotPosition pos, GuiElement element) {
         this.getElements().put(pos, element);
+    }
+    public void addFlag(GuiFlag flag) {
+        this.getFlags().add(flag);
+    }
+    public void addFlags(GuiFlag... flags) {
+        this.getFlags().addAll(List.of(flags));
     }
     public void addLayer(GuiLayer layer) {
         this.getLayers().add(layer);
