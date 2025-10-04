7cb4f858c5fc00942a827a6110e93072dc1144e2
darksoulq <amaanwani1a@gmail.com>
2025-10-04 17:01:14 +0530

    New: - Entity#addSpawnWeigth now takes in weight as a float 0-1f range. - Summon command for summoning custom entities

--- src/main/java/com/github/darksoulq/abyssallib/server/command/internal/InternalCommand.java
+++ src/main/java/com/github/darksoulq/abyssallib/server/command/internal/InternalCommand.java
@@ -2,7 +2,9 @@ package com.github.darksoulq.abyssallib.server.command.internal;
 
 import com.github.darksoulq.abyssallib.server.command.Command;
 import com.github.darksoulq.abyssallib.server.command.CommandBus;
+import com.github.darksoulq.abyssallib.server.event.custom.entity.EntitySpawnEvent;
 import com.github.darksoulq.abyssallib.server.registry.Registries;
+import com.github.darksoulq.abyssallib.world.entity.Entity;
 import com.github.darksoulq.abyssallib.world.entity.data.EntityAttributes;
 import com.github.darksoulq.abyssallib.world.item.Item;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
@@ -14,9 +16,11 @@ import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
 import io.papermc.paper.command.brigadier.argument.resolvers.selector.EntitySelectorArgumentResolver;
+import io.papermc.paper.math.FinePosition;
+import org.bukkit.Location;
 import org.bukkit.NamespacedKey;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 
 import java.util.List;
@@ -47,9 +51,24 @@ public class InternalCommand {
                                 )
                         )
                 )
+                .then(Commands.literal("summon")
+                        .requires(sender -> sender.getSender().hasPermission("abyssallib.admin.summon"))
+                        .then(Commands.argument("location", ArgumentTypes.finePosition(false))
+                                .then(Commands.argument("namespace_id", ArgumentTypes.namespacedKey())
+                                        .executes(ctx -> {
+                                            try {
+                                                return summonExecutor(ctx);
+                                            } catch (CloneNotSupportedException e) {
+                                                throw new RuntimeException(e);
+                                            }
+                                        })
+                                        .suggests(InternalCommand::summonSuggests)
+                                )
+                        )
+                )
                 .then(Commands.literal("reload")
                         .requires(sender -> sender
-                                .getSender().hasPermission("abyssallib.admin.give"))
+                                .getSender().hasPermission("abyssallib.admin.reload"))
                         .then(Commands.literal("commmands")
                                 .executes(ctx -> {
                                     CommandBus.reloadAll();
@@ -62,7 +81,7 @@ public class InternalCommand {
     private static int giveExecutor(CommandContext<CommandSourceStack> ctx) {
         NamespacedKey namespaceId = ctx.getArgument("namespace_id", NamespacedKey.class);
         CommandSender sender = ctx.getSource().getSender();
-        Entity executor = ctx.getSource().getExecutor();
+        org.bukkit.entity.Entity executor = ctx.getSource().getExecutor();
 
         if (!(executor instanceof Player player)) {
             sender.sendPlainMessage("Only a player can run this command!");
@@ -88,7 +107,7 @@ public class InternalCommand {
     public static int attributeGetExecutor(CommandContext<CommandSourceStack> ctx) {
         NamespacedKey key = ctx.getArgument("type", NamespacedKey.class);
         EntitySelectorArgumentResolver selector = ctx.getArgument("selector", EntitySelectorArgumentResolver.class);
-        List<Entity> entities;
+        List<org.bukkit.entity.Entity> entities;
         try {
             entities = selector.resolve(ctx.getSource());
             if (!entities.isEmpty()) {
@@ -108,7 +127,7 @@ public class InternalCommand {
                                                                        final SuggestionsBuilder builder) {
         EntitySelectorArgumentResolver selector = ctx.getArgument("selector", EntitySelectorArgumentResolver.class);
         try {
-            List<Entity> entities = selector.resolve(ctx.getSource());
+            List<org.bukkit.entity.Entity> entities = selector.resolve(ctx.getSource());
             if (!entities.isEmpty()) {
                 EntityAttributes data = EntityAttributes.of(entities.getFirst());
                 data.load();
@@ -120,6 +139,28 @@ public class InternalCommand {
         } catch (CommandSyntaxException e) {
             throw new RuntimeException(e);
         }
+        return builder.buildFuture();
+    }
+
+    public static int summonExecutor(CommandContext<CommandSourceStack> ctx) throws CloneNotSupportedException {
+        FinePosition position = ctx.getArgument("locationn", FinePosition.class);
+        Location loc = position.toLocation(ctx.getSource().getExecutor().getWorld());
+        NamespacedKey id = ctx.getArgument("namespace_id", NamespacedKey.class);
+
+        if (!Registries.ENTITIES.contains(id.asString())) {
+            ctx.getSource().getExecutor().sendPlainMessage("Not an entity");
+            return com.mojang.brigadier.Command.SINGLE_SUCCESS;
+        }
+
+        Registries.ENTITIES.get(id.asString()).clone().spawn(loc, EntitySpawnEvent.SpawnReason.PLUGIN);
+        return Command.SUCCESS;
+    }
+    public static CompletableFuture<Suggestions> summonSuggests(final CommandContext<CommandSourceStack> ctx,
+                                                              final SuggestionsBuilder builder) {
+        for (Entity<? extends LivingEntity> entity : Registries.ENTITIES.getAll().values()) {
+            builder.suggest(entity.getId().toString());
+        }
+
         return builder.buildFuture();
     }
 }
