fdb4855602c94de2e6d73ca73bfe83103b37f6f8
darksoulq <amaanwani1a@gmail.com>
2025-11-24 21:22:42 +0530

    add Deprecated to CustomRecipe

--- src/main/java/com/github/darksoulq/abyssallib/world/recipe/CustomRecipe.java
+++ src/main/java/com/github/darksoulq/abyssallib/world/recipe/CustomRecipe.java
@@ -1,7 +1,10 @@
 package com.github.darksoulq.abyssallib.world.recipe;
 
 import com.github.darksoulq.abyssallib.common.util.Identifier;
+import org.jetbrains.annotations.ApiStatus;
 
+@Deprecated
+@ApiStatus.ScheduledForRemoval(inVersion = "1.7.0-mc1.21.10")
 public abstract class CustomRecipe<T extends CustomRecipe<T>> {
     private final Identifier id;
 
