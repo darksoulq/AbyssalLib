3a6b7af9ee17cf08d50d14a7e73ca4ae8b22a0a1
darksoulq <amaanwani1a@gmail.com>
2026-08-17 11:59:24 +0530

    Added 2 new json specific merge strategies to replace existing vanilla-overriding loot tables

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
 
 group = "io.github.darksoulq"
 val mcVersion = stonecutter.current.project
-version = "2.4.0-mc.${mcVersion}-alpha.4"
+version = "2.4.0-mc.${mcVersion}-alpha.5"
 
 val yamlParser = Yaml()
 
